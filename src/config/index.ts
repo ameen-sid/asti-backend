64be550cd4fee678e02eef9495f03952fc3c28c2
Ameen Sid <ameensid7@outlook.com>
2026-08-17 15:36:16 +0530

    Added config file for dotenv

---
+++ src/config/index.ts
@@ -0,0 +1,23 @@
+import dotenv from 'dotenv';
+
+type ServerConfig = {
+  PORT: number;
+  DATABASE_URL: string;
+  NODE_ENV: string;
+  JWT_SECRET: string;
+  JWT_EXPIRES_IN: string;
+};
+
+const loadEnv = () => {
+  dotenv.config();
+  console.log(`Environment Variables are Loaded...`);
+};
+loadEnv();
+
+export const serverConfig: ServerConfig = {
+  PORT: Number(process.env.PORT) || 3001,
+  DATABASE_URL: process.env.DATABASE_URL || '',
+  NODE_ENV: process.env.NODE_ENV || 'development',
+  JWT_SECRET: process.env.JWT_SECRET || 'asti_super_secret_key_123',
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '24h',
+};
