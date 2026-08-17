63d538d261a298dc9de313995c4392ccaa4ea24b
Ameen Sid <ameensid7@outlook.com>
2026-08-17 15:38:19 +0530

    Added correlation middleware

---
+++ src/middleware/correlation.middleware.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from 'express';
+import { v4 as uuidV4 } from 'uuid';
+import { asyncLocalStorage } from '../shared/utils/helpers/request.helpers';
+
+export const attachCorrelationIdMiddleware = (req: Request, res: Response, next: NextFunction) => {
+  const correlationId = uuidV4();
+  req.headers['x-correlation-id'] = correlationId;
+  asyncLocalStorage.run({ correlationId: correlationId }, () => {
+    next();
+  });
+};
