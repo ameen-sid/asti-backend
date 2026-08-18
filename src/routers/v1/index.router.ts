dd637c1f6e6abde11c91b28d7fad18bd4cd01e4e
Ameen Sid <ameensid7@outlook.com>
2026-08-18 17:40:30 +0530

    Added deparment, subDeparment, section, line and machine routes in v1 router

--- src/routers/v1/index.router.ts
+++ src/routers/v1/index.router.ts
@@ -1,5 +1,17 @@
 import express from 'express';
+import departmentRouter from '../../modules/lms/department/department.routes';
+import subDepartmentRouter from '../../modules/lms/sub-department/sub-department.routes';
+import sectionRouter from '../../modules/lms/section/section.routes';
+import lineRouter from '../../modules/lms/line/line.routes';
+import machineRouter from '../../modules/lms/machine/machine.routes';
+import { authenticateToken } from '../../middleware/auth.middleware';
 
 const v1Router = express.Router();
+
+v1Router.use('/departments', authenticateToken, departmentRouter);
+v1Router.use('/sub-departments', authenticateToken, subDepartmentRouter);
+v1Router.use('/sections', authenticateToken, sectionRouter);
+v1Router.use('/lines', authenticateToken, lineRouter);
+v1Router.use('/machines', authenticateToken, machineRouter);
 
 export default v1Router;
